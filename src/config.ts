00bd8b600c8b00f048a0de0e0f3cd60966c34d1b
yizhibaimu666 <yizhibaimu666@gmail.com>
2026-05-17 11:23:59 +0800

    finish

--- src/config.ts
+++ src/config.ts
@@ -290,27 +290,27 @@ export const navBarConfig: NavBarConfig = {
 			url: "/content/",
 			icon: "material-symbols:person",
 			children: [
-				{
-					name: "Anime",
-					url: "/anime/",
-					icon: "material-symbols:movie",
-				},
-				{
-					name: "Diary",
-					url: "/diary/",
-					icon: "material-symbols:book",
-				},
-				{
-					name: "Gallery",
-					url: "/albums/",
-					icon: "material-symbols:photo-library",
-				},
-				{
-					name: "Devices",
-					url: "/devices/",
-					icon: "material-symbols:devices",
-					external: false,
-				},
+				//{
+				//	name: "Anime",
+				//	url: "/anime/",
+				//	icon: "material-symbols:movie",
+				//},
+				//{
+				//	name: "Diary",
+				//	url: "/diary/",
+				//	icon: "material-symbols:book",
+				//},
+				//{
+				//	name: "Gallery",
+				//	url: "/albums/",
+				//	icon: "material-symbols:photo-library",
+				//},
+				//{
+				//	name: "Devices",
+				//	url: "/devices/",
+				//	icon: "material-symbols:devices",
+				//	external: false,
+				//},
 			],
 		},
 		{
@@ -318,16 +318,16 @@ export const navBarConfig: NavBarConfig = {
 			url: "/content/",
 			icon: "material-symbols:info",
 			children: [
-				{
-					name: "About",
-					url: "/about/",
-					icon: "material-symbols:person",
-				},
-				{
-					name: "Friends",
-					url: "/friends/",
-					icon: "material-symbols:group",
-				},
+				//{
+				//	name: "About",
+				//	url: "/about/",
+				//	icon: "material-symbols:person",
+				//},
+				//{
+				//	name: "Friends",
+				//	url: "/friends/",
+				//	icon: "material-symbols:group",
+				//},
 			],
 		},
 		{
@@ -335,21 +335,21 @@ export const navBarConfig: NavBarConfig = {
 			url: "#",
 			icon: "material-symbols:more-horiz",
 			children: [
-				{
-					name: "Projects",
-					url: "/projects/",
-					icon: "material-symbols:work",
-				},
-				{
-					name: "Skills",
-					url: "/skills/",
-					icon: "material-symbols:psychology",
-				},
-				{
-					name: "Timeline",
-					url: "/timeline/",
-					icon: "material-symbols:timeline",
-				},
+				//{
+				//	name: "Projects",
+				//	url: "/projects/",
+				//	icon: "material-symbols:work",
+				//},
+				//{
+				//	name: "Skills",
+				//	url: "/skills/",
+				//	icon: "material-symbols:psychology",
+				//},
+				//{
+				//	name: "Timeline",
+				//	url: "/timeline/",
+				//	icon: "material-symbols:timeline",
+				//},
 			],
 		},
 	],
@@ -620,7 +620,7 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 export const sakuraConfig: SakuraConfig = {
 	enable: true, // 默认关闭樱花特效
 	sakuraNum: 30, // 樱花数量
-	limitTimes: 10, // 樱花越界限制次数，-1为无限循环
+	limitTimes: 5, // 樱花越界限制次数，-1为无限循环
 	size: {
 		min: 0.5, // 樱花最小尺寸倍数
 		max: 1.1, // 樱花最大尺寸倍数
