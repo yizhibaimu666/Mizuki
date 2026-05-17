33f1ddabbb3caf9243234f9e1dea81542551926c
yizhibaimu666 <yizhibaimu666@gmail.com>
2026-05-18 00:00:36 +0800

    ,

--- src/config.ts
+++ src/config.ts
@@ -476,7 +476,7 @@ export const musicPlayerConfig: MusicPlayerConfig = {
 };
 
 export const footerConfig: FooterConfig = {
-	enable: false, // 是否启用Footer HTML注入功能
+	enable: true, // 是否启用Footer HTML注入功能
 	customHtml: "", // HTML格式的自定义页脚信息，例如备案号等，默认留空
 	// 也可以直接编辑 FooterConfig.html 文件来添加备案号等自定义内容
 	// 注意：若 customHtml 不为空，则使用 customHtml 中的内容；若 customHtml 留空，则使用 FooterConfig.html 文件中的内容
